4fdcd489e74b7736660031e03a20ea27a0f56243
ivanh <rik.chanel@gmail.com>
2022-11-18 16:34:39 +0300

    open cards

--- app/src/main/java/com/example/pairs/Board.java
+++ app/src/main/java/com/example/pairs/Board.java
@@ -4,25 +4,25 @@ public class Board {
     private int stepsCount;
     private int stepsFinish;
     private boolean winOrLose;
-    private int[][] board = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    private int[][] board = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
     private int[] cards = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
 
     public Board() {
         int tempNumber;
         int i = 0, j = 0;
-        while (this.board[i][j] == 0) {
+        while (this.board[i][j] == -1) {
             tempNumber = (int) (Math.random() * 12);
-            if (cards[tempNumber] != 0) {
+            if (cards[tempNumber] != -1) {
                 this.board[i][j] = cards[tempNumber];
-                cards[tempNumber] = 0;
+                cards[tempNumber] = -1;
             } else {
                 int ii = 0;
-                while (cards[ii] == 0) {
+                while (cards[ii] == -1) {
                     ii++;
                     tempNumber = ii;
                 }
                 this.board[i][j] = cards[tempNumber];
-                cards[tempNumber] = 0;
+                cards[tempNumber] = -1;
             }
             j++;
             if (j == 3) {
@@ -33,9 +33,6 @@ public class Board {
                 break;
             }
         }
-
-
-
     }
     public int[][] getBoard() {
         return board;
--- app/src/main/java/com/example/pairs/MainActivity.java
+++ app/src/main/java/com/example/pairs/MainActivity.java
@@ -2,13 +2,106 @@ package com.example.pairs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11;
+    Board board;
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        board = new Board();
+        board.getBoard();
+        /*intilize views*/
+        img0 = findViewById(R.id.imageView0);
+        img1 = findViewById(R.id.imageView1);
+        img2 = findViewById(R.id.imageView2);
+        img3 = findViewById(R.id.imageView3);
+        img4 = findViewById(R.id.imageView4);
+        img5 = findViewById(R.id.imageView5);
+        img6 = findViewById(R.id.imageView6);
+        img7 = findViewById(R.id.imageView7);
+        img8 = findViewById(R.id.imageView8);
+        img9 = findViewById(R.id.imageView9);
+        img10 = findViewById(R.id.imageView10);
+        img11 = findViewById(R.id.imageView11);
+
+
+        /*set on click listener*/
+        img0.setOnClickListener(this);
+        img1.setOnClickListener(this);
+        img2.setOnClickListener(this);
+        img3.setOnClickListener(this);
+        img4.setOnClickListener(this);
+        img5.setOnClickListener(this);
+        img6.setOnClickListener(this);
+        img7.setOnClickListener(this);
+        img8.setOnClickListener(this);
+        img9.setOnClickListener(this);
+        img10.setOnClickListener(this);
+        img11.setOnClickListener(this);
+
+
+        /*set tag*/
+        img0.setTag("0");
+        img1.setTag("1");
+        img2.setTag("2");
+        img3.setTag("3");
+        img4.setTag("4");
+        img5.setTag("5");
+        img6.setTag("6");
+        img7.setTag("7");
+        img8.setTag("8");
+        img9.setTag("9");
+        img10.setTag("10");
+        img11.setTag("11");
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (Integer.parseInt((String) view.getTag())) {
+            case 0 :
+                img0.setImageDrawable( GetImg(board.getBoard(), Integer.parseInt((String) view.getTag()) / 3, Integer.parseInt((String) view.getTag()) % 3));
+                break;
+            case 1 :
+                img1.setImageDrawable( GetImg(board.getBoard(), Integer.parseInt((String) view.getTag()) / 3, Integer.parseInt((String) view.getTag()) % 3));
+                break;
+        }
+    }
+
+
+    public Drawable GetImg(int[][] board, int i, int j) {
+        Drawable drawableCard = getDrawable(R.drawable.card);
+        switch (board[i][j]) {
+            case 0:
+                drawableCard = getDrawable(R.drawable.ace);
+                break;
+            case 1:
+                drawableCard = getDrawable(R.drawable.king);
+                break;
+            case 2:
+                drawableCard = getDrawable(R.drawable.queen);
+                break;
+            case 3:
+                drawableCard = getDrawable(R.drawable.jack);
+                break;
+            case 4:
+                drawableCard = getDrawable(R.drawable.ten);
+                break;
+            case 5:
+                drawableCard = getDrawable(R.drawable.joker);
+                break;
+        }
+        return drawableCard;
     }
 }
